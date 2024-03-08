14297d71c151ec337e5b5cd63b8205a47a5bab06
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-09 07:27:49 +0800

    progress

---
+++ src/store/cart-slice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find(item => id === item.id)
+    },
+    removeItemToCart(state) {
+
+    }
+  },
+});
