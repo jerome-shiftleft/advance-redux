dffe6449f6f79c09c4b652323581e3e5347e56b8
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-09 08:05:17 +0800

    progress

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -8,9 +8,21 @@ const cartSlice = createSlice({
   },
   reducers: {
     addItemToCart(state, action) {
-    const newItem = action.payload;
-    state.items.find((item) => newItem.id === item.id);
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => newItem.id === item.id);
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice += newItem.price;
+      }
+    },
+    removeItemToCart(state) {},
   },
-  removeItemToCart(state) {},
-},
 });
